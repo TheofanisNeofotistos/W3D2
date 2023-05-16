061473b35a52b1737f3bb8a0ba0bef51bd11c0f5
Theofanis Neofotistos <neofotistostheofanis@gmail.com>
2023-05-16 14:31:31 -0400

    Iteration 3

--- practice-for-ch-ruby-memory-long-practice-main/memory_game/Board.rb
+++ practice-for-ch-ruby-memory-long-practice-main/memory_game/Board.rb
@@ -3,7 +3,25 @@ require_relative "Card.rb"
 class Board
    
 def initialize
-  @grid = Array.new
+  @grid = Array.new(4){Array.new(4," ")}
+
+end
+
+def populate
+   dictionary = Array.new(13){|i| i+1}.sample(8)
+   new_dictionary = (dictionary += dictionary).shuffle
+  x = 0
+  (0...@grid.length).each do |i|
+    (0...@grid.length).each do |j| 
+      @grid[i][j] = new_dictionary[x]
+      x += 1
+    end
+  end
+   @grid
+end 
+
+def render 
+  @grid.each{|ele| p ele.join(" ")}
 end
 
 end
